b0ad96efe64f808a131091db559e8daf9d035d4d
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-06-09 21:07:49 -0400

    add form feature for url

--- media_convertor/forms.py
+++ media_convertor/forms.py
@@ -2,4 +2,12 @@ from django import forms
 
 
 class DocumentForm(forms.Form):
-    mediafile = forms.FileField(label='Select a file')
+    mediafile = forms.FileField(label='Select a file', required=False)
+    mediaUrl = forms.CharField(label='Paste Wav file Url', max_length=1000, required=False)
+
+
+def has_media_file():
+    return False
+
+def has_media_url():
+    return True
--- media_convertor/views.py
+++ media_convertor/views.py
@@ -2,6 +2,7 @@ import os
 import mimetypes
 import codecs
 import time
+import requests
 
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -14,7 +15,7 @@ def home(request):
     message = ''
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
+        if ("mediafile" in request.FILES):
             inputFile = Document(mediaFile=request.FILES['mediafile'])
             inputFile.save()
 
@@ -24,15 +25,46 @@ def home(request):
 
             input_path = inputFile.mediaFile.path
             output_path = f"{output_dir}{output_filename}"
-            run(input_path, output_path)
+            if input_path.endswith('.wav'):
+                run(input_path, output_path)
 
-            midFile = open(output_path, 'rb')
-            response = HttpResponse(midFile, content_type='audio/mid')
+                midFile = open(output_path, 'rb')
+                response = HttpResponse(midFile, content_type='audio/mid')
 
-            response['Content-Disposition'] = "attachment; filename=%s" % output_filename
-            return response
+                response['Content-Disposition'] = "attachment; filename=%s" % output_filename
+                return response
+            else:
+                message = 'Invalid File Format!'
+
+        elif (form.data['mediaUrl'] != ''): #form.has_media_url():
+            url = form.data['mediaUrl']
+
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+
+            input_filename = f"input_{timestr}.wav"
+            input_path = f"media/{input_filename}"
+
+            file_response = requests.get(url)
+            if (url.endswith('.wav') and file_response.status_code == 200):
+                open(input_path, "wb").write(file_response.content)
+
+                output_filename = f"output_{timestr}.mid"
+                output_dir = "media/"
+
+                output_path = f"{output_dir}{output_filename}"
+                run(input_path, output_path)
+
+                midFile = open(output_path, 'rb')
+                response = HttpResponse(midFile, content_type='audio/mid')
+
+                response['Content-Disposition'] = "attachment; filename=%s" % output_filename
+                return response
+            else:
+                message = 'Invalid Url!'
+                # form = DocumentForm()
         else:
             message = 'Invalid form!'
+            # form = DocumentForm()
     else:
         form = DocumentForm()
 
