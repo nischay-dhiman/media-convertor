306f0d7cca58dd5d9d831b10b645692fbc6833c3
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-06-09 21:51:38 -0400

    added css changes in home UI

--- media_convertor/forms.py
+++ media_convertor/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class DocumentForm(forms.Form):
     mediafile = forms.FileField(label='Select a file', required=False)
-    mediaUrl = forms.CharField(label='Paste Wav file Url', max_length=1000, required=False)
+    mediaUrl = forms.CharField(label='Paste Wav file Url', max_length=1000, required=False, widget=forms.TextInput(attrs={'placeholder': 'Paste .wav url'}))
 
 
 def has_media_file():
